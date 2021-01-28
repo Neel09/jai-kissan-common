b7087d29027733ab16b844e3ffbd00b4fade09d8
Neel09 <neelansh.gupta080@gmail.com>
2021-01-29 02:05:55 +0530

    Creating String Prefixed Sequence Generator

---
+++ src/main/java/org/jai/kissan/commonutils/CommonUtilsApplication.java
@@ -0,0 +1,13 @@
+package org.jai.kissan.commonutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jai/kissan/commonutils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jai.kissan.commonutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
